7b4c22eb75076f4ebf6c94afc3d0b2709504483a
Maria Belyalova <mashabelyalova@gmail.com>
2018-02-18 13:54:44 +0300

    project

---
+++ PriceRecognition/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PriceRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter a file path");
+                return;
+            }
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Too much arguments");
+                return;
+            }
+
+            if (File.Exists(args[0]))
+            {
+                Recognizer rec = new Recognizer(args);
+                rec.Preprocess();
+                rec.ImageToText();
+                List<string> prices = rec.FindPrices();
+                if (prices.Count == 0)
+                {
+                    Console.WriteLine("There are no prices found");
+                }
+                else
+                {
+                    foreach (string price in prices)
+                    {
+                        Console.Write(price + ' ');
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a valid file", args[0]);
+            }
+        }
+    }
+}
---
+++ PriceRecognition/Recognizer.cs
@@ -0,0 +1,108 @@
+﻿using Puma.Net;
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace PriceRecognition
+{
+    class Recognizer
+    {
+        string imageText;
+        Bitmap picture;
+        OtsuThreshold otsu;
+        public Recognizer(string[] arg)
+        {
+            picture = new Bitmap(arg[0]);
+            otsu = new OtsuThreshold();
+        }
+        public void Preprocess()
+        {
+            Bitmap temp = (Bitmap)picture.Clone();
+            otsu.Convert2GrayScale(temp);
+            int threshold = otsu.getOtsuThreshold((Bitmap)temp);
+            otsu.thresholdImage(temp, threshold);
+            picture = (Bitmap)temp.Clone();
+        }
+        public void ImageToText()
+        {
+            string result = "";
+            //picture.Save("picture.png");
+            PumaPage image = new PumaPage(picture);
+            using (image)
+            {
+                image.FileFormat = PumaFileFormat.TxtAscii;
+                image.AutoRotateImage = false;
+                image.EnableSpeller = false;
+                image.Language = PumaLanguage.RussianEnglish;
+                image.PreserveLineBreaks = false;
+
+                try
+                {
+                    result = image.RecognizeToString();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Recognition failed");
+                }
+                finally
+                {
+                    image.Dispose();
+                }
+            }
+
+            picture.Dispose();
+            result = result.Replace(" ", "");
+            imageText = String.Copy(result);
+        }
+        public List<string> FindPrices()
+        {
+            List<string> prices = new List<string>();
+
+            string[] patterns = new string[2];
+            patterns[0] = @"(Э|Е|E|f|\$|EUR|€|£)[0-9]{1,}(.?|,?)[0-9]{1,}";
+            patterns[1] = @"[0-9]{1,}(.?|,?)[0-9]{1,}(P|p|Р|р|\$|€|£)";
+
+            foreach (string pattern in patterns)
+            {
+                try
+                {
+                    foreach (Match m in Regex.Matches(imageText, pattern))
+                    {
+                        string val = String.Copy(m.Value);
+                        if (val.Contains("Э"))
+                        {
+                            val = val.Replace("Э", "e");
+                        }
+                        if (val.Contains("EUR"))
+                        {
+                            val = val.Replace("EUR", "e");
+                        }
+                        if (val.Contains("eur"))
+                        {
+                            val = val.Replace("eur", "e");
+                        }
+                        if (val.Contains("Е")) //russian
+                        {
+                            val = val.Replace("Е", "f");
+                        }
+                        if (val.Contains("E")) //english
+                        {
+                            val = val.Replace("E", "f");
+                        }
+                        prices.Add(val);
+                    }
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+
+            return prices;
+        }
+    }
+}
